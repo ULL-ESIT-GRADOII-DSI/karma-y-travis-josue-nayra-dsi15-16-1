1f46274e9a1d612b3b9861cac092b83a0ce41e3f
josuetc94 <alu0100763492@ull.edu.es>
2016-03-27 19:22:42 +0000

    Nuevas expectativas para la clase Medida. Comparaciones

--- vendor/tests.js
+++ vendor/tests.js
@@ -45,4 +45,40 @@ describe('Medida', function() {
         expect(medida1).to.have.property('tipo');
       });
   });
+  describe('Comparando las medidas',function()
+    {
+      it('El valor y tipo de los objetos medida1 y medida2 son iguales',function()
+      {
+        expect(medida1.valor).to.be.equal(medida2.valor);
+        expect(medida1.tipo).to.be.equal(medida2.tipo);
+      });
+      it('El valor y tipo de los objetos medida3 y medida1 o medida2 son diferentes',function()
+      {
+        expect(medida3.valor).to.not.be.equal(medida1.valor);
+        expect(medida3.valor).to.not.be.equal(medida2.valor);
+        expect(medida3.tipo).to.not.be.equal(medida1.tipo);
+        expect(medida3.tipo).to.not.be.equal(medida2.tipo);
+    
+      });
+      it('El valor del objeto medida3 es mayor que el valor del objeto medida1',function()
+      {
+        expect(medida1.valor).to.not.be.above(medida3.valor);
+        expect(medida3.valor).to.be.above(medida2.valor);
+      });  
+      it('El valor del objeto medida1 es menor que el valor del objeto medida3',function()
+      {
+        expect(medida1.valor).to.be.below(medida3.valor);
+      }); 
+      it('El valor de los objetos medida1, medida2 y medida3 esta dentro del rango 40-100',function()
+      {
+        expect(medida1.valor).to.be.within(40,100);
+        expect(medida2.valor).to.be.within(40,100);
+        expect(medida3.valor).to.be.within(40,100);
+      });
+      it('El valor de los objetos medida1 y medida2 no esta dentro del rango 50-100',function()
+      {
+        expect(medida1.valor).to.not.be.within(50,100);
+        expect(medida2.valor).to.not.be.within(50,100);
+      });
+    });
 });
