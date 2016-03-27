86e5ed5216194ee9fb6e21519486b6b4c6a627ae
alu0100406122 <alu0100406122@ull.edu.es>
2016-03-27 19:47:50 +0000

    Pull de los cambios nuevos.

--- vendor/tests.js
+++ vendor/tests.js
@@ -8,8 +8,8 @@ describe('Medida', function() {
   var medida3 = new Medida("77 m");
   
   
-  describe('Construccion',function()
-  {
+    describe('Construccion',function()
+    {
       it('Se crean los objetos medida1, medida2, medida3',function()
       {
         expect(medida1).to.be.an('object');
@@ -44,45 +44,43 @@ describe('Medida', function() {
         expect(medida1).to.have.property('valor');
         expect(medida1).to.have.property('tipo');
       });
-  });
-
-  describe('Comparando las medidas',function()
-    {
-      it('El valor y tipo de los objetos medida1 y medida2 son iguales',function()
-      {
-        expect(medida1.valor).to.be.equal(medida2.valor);
-        expect(medida1.tipo).to.be.equal(medida2.tipo);
-      });
-      it('El valor y tipo de los objetos medida3 y medida1 o medida2 son diferentes',function()
-      {
-        expect(medida3.valor).to.not.be.equal(medida1.valor);
-        expect(medida3.valor).to.not.be.equal(medida2.valor);
-        expect(medida3.tipo).to.not.be.equal(medida1.tipo);
-        expect(medida3.tipo).to.not.be.equal(medida2.tipo);
-    
-      });
-      it('El valor del objeto medida3 es mayor que el valor del objeto medida1',function()
-      {
-        expect(medida1.valor).to.not.be.above(medida3.valor);
-        expect(medida3.valor).to.be.above(medida2.valor);
-      });  
-      it('El valor del objeto medida1 es menor que el valor del objeto medida3',function()
-      {
-        expect(medida1.valor).to.be.below(medida3.valor);
-      }); 
-      it('El valor de los objetos medida1, medida2 y medida3 esta dentro del rango 40-100',function()
-      {
-        expect(medida1.valor).to.be.within(40,100);
-        expect(medida2.valor).to.be.within(40,100);
-        expect(medida3.valor).to.be.within(40,100);
-      });
-      it('El valor de los objetos medida1 y medida2 no esta dentro del rango 50-100',function()
-      {
-        expect(medida1.valor).to.not.be.within(50,100);
-        expect(medida2.valor).to.not.be.within(50,100);
-      });
     });
-    
+
+    describe('Comparando las medidas',function()
+    {
+        it('El valor y tipo de los objetos medida1 y medida2 son iguales',function()
+        {
+            expect(medida1.valor).to.be.equal(medida2.valor);
+            expect(medida1.tipo).to.be.equal(medida2.tipo);
+        });
+        it('El valor y tipo de los objetos medida3 y medida1 o medida2 son diferentes',function()
+        {
+            expect(medida3.valor).to.not.be.equal(medida1.valor);
+            expect(medida3.valor).to.not.be.equal(medida2.valor);
+            expect(medida3.tipo).to.not.be.equal(medida1.tipo);
+            expect(medida3.tipo).to.not.be.equal(medida2.tipo);
+        });
+        it('El valor del objeto medida3 es mayor que el valor del objeto medida1',function()
+        {
+            expect(medida1.valor).to.not.be.above(medida3.valor);
+            expect(medida3.valor).to.be.above(medida2.valor);
+        });  
+        it('El valor del objeto medida1 es menor que el valor del objeto medida3',function()
+        {
+            expect(medida1.valor).to.be.below(medida3.valor);
+        }); 
+        it('El valor de los objetos medida1, medida2 y medida3 esta dentro del rango 40-100',function()
+        {
+            expect(medida1.valor).to.be.within(40,100);
+            expect(medida2.valor).to.be.within(40,100);
+            expect(medida3.valor).to.be.within(40,100);
+        });
+        it('El valor de los objetos medida1 y medida2 no esta dentro del rango 50-100',function()
+        {
+            expect(medida1.valor).to.not.be.within(50,100);
+            expect(medida2.valor).to.not.be.within(50,100);
+        });
+    });
     
     describe('Pruebas y casos',function()
     {
@@ -117,6 +115,9 @@ describe('Medida', function() {
             var aux = "1z"; //No especificamos destino alguno.
             expect(Medida.convertir(aux)).to.be.equal("Introduzca una temperatura valida: 330e-1 F to C");
         });
-  });
+    });
+
+    //Pruebas para la clase Temperatura.
+
 
 });
