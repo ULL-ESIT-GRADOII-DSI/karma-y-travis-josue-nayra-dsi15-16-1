31af5878ad229e80e16215a4f00608735b643114
alu0100406122 <alu0100406122@ull.edu.es>
2016-03-27 19:30:59 +0000

    Añadiendo nuevas expectativas para la clase Medida.

--- vendor/tests.js
+++ vendor/tests.js
@@ -24,7 +24,7 @@ describe('Medida', function() {
       });
       it('Se construye medida pasandole al constructor: Valor y Tipo', function () {
         expect(medida1).to.not.be.null;
-        expect(medida1.valor).to.equal(45);
+        expect(medida1.valor).to.equal("45");
         expect(medida1.tipo).to.equal("km");
       });
       it('Se construye medida pasandole al constructor: Valor',function(){
@@ -45,4 +45,45 @@ describe('Medida', function() {
         expect(medida1).to.have.property('tipo');
       });
   });
+  
+  describe('Pruebas y casos',function()
+  {
+    it('Medida1, medida2 y medida3 hacen match con la regexp',function()
+    {
+      var auxiliar = medida1.valor + " " + medida1.tipo;
+      var auxiliar1 = medida2.valor + " " + medida2.tipo;
+      var auxiliar2 = medida3.valor + " " + medida3.tipo;
+      expect(auxiliar).to.match(/^\s*([-+]?\d+(?:\.\d*)?(?:e[+-]?\d+)?)\s*[a-z][a-z0-9]*$/i);
+      expect(auxiliar1).to.match(/^\s*([-+]?\d+(?:\.\d*)?(?:e[+-]?\d+)?)\s*[a-z][a-z0-9]*$/i);
+      expect(auxiliar2).to.match(/^\s*([-+]?\d+(?:\.\d*)?(?:e[+-]?\d+)?)\s*[a-z][a-z0-9]*$/i);
+    });
+    it('Creando objeto Medida con un solo parámetro',function()
+    {
+        var aux = new Medida("50c");
+        expect(aux.valor).to.be.equal(50);
+        expect(aux.tipo).to.be.equal("c");
+    }); 
+    it('Devolviendo desconozco como se puede... en función convertir',function()
+    {
+        var aux = "1p to z";
+        var res = Medida.match(aux);
+        var tipo = res.tipo;
+        var destino = res.to;
+        console.log("Tipo->"+tipo);
+        console.log("Destino->"+destino);
+        
+        expect(Medida.convertir("1p to z")).to.be.equal("Desconozco como convertir desde "+tipo+" hasta "+destino); 
+    });
+    it('Devolviendo Introduzca una temperatura...',function()
+    {
+        var aux = "1z"; //No especificamos destino alguno.
+        expect(Medida.convertir(aux)).to.be.equal("Introduzca una temperatura valida: 330e-1 F to C");
+    });
+    
+  });
+  
+  
+  
+  
+  
 });
